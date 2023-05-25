6e658ccb190c047c3fcef6cf966c001b26433197
MACIRE BERETE <76943805+MACIRE05@users.noreply.github.com>
2023-05-25 16:37:02 +0200

    Add files via upload

---
+++ Projet_mini-application.py/main.py
@@ -0,0 +1,16 @@
+from myapp import MyApp
+
+def main():
+    app = MyApp()
+    app.launch()
+    app.use()
+    app.quit()
+
+if __name__ == '__main__':
+    main()
+
+
+// Commentaire sur le contenu des autres modules
+// #include "myapp.h" // Définition de la classe MyApp
+// #include "student.h" // Définition de la structure Student
+// #include "teacher.h" // Définition de la structure Teacher
